f8b4426267bb1477edc87fa06fe249691a78122c
jamespost <32137126+jamespost@users.noreply.github.com>
2020-04-11 15:08:49 -0500

    Successful Prototype of Magnetic attraction

---
+++ Assets/Scripts/Magnetism.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Magnetism needs to make sure that the parent game object has a Sphere Collider that represents its Magnetic Field
+[RequireComponent(typeof(SphereCollider))]
+public class Magnetism : MonoBehaviour
+{
+    ///A Class to define magnetic behavior between game objects
+    ///
+
+    //Magnetic Strength of the Magnet (in Gauss)
+    public float gauss;
+
+    //Magnetism needs to make sure that the parent game object has a Sphere Collider that represents its Magnetic Field
+    SphereCollider magneticFieldSphere;
+
+    private void Start()
+    {
+        magneticFieldSphere = GetComponent<SphereCollider>();
+        ConfigureSphereCollider();
+    }
+
+    //Methods
+    //Applies a pulling Force on any Magnetic Game Object within the bounds of magneticFieldSphere
+    private void ExertMagneticForce(GameObject g)
+    {
+        //get the rigidbody component of g
+        Rigidbody rb = g.GetComponent<Rigidbody>();
+        //face the transform of rb towards the Magnet
+        rb.gameObject.transform.LookAt(transform);
+        //add force to rb in the direction it is facing
+        rb.AddForce(rb.transform.forward * gauss, ForceMode.Force);
+    }
+
+    
+    private void OnTriggerStay(Collider collision)
+    {
+
+
+        if (collision.gameObject.GetComponent<PhysicalProperties>().isMagnetic)
+        {
+            //run the ExertMagneticForce method on the gameObject parent of the collision
+            ExertMagneticForce(collision.gameObject);
+
+            //debug
+            Debug.Log("There is a magnetic game object within magneticFieldSphere");
+        }
+    }
+
+    //Sets the radius of the game objects magneticFieldSphere
+    private void SetMagneticFieldRadius()
+    {
+        magneticFieldSphere.radius = gameObject.GetComponentInParent<Rigidbody>().mass;
+    }
+
+    //Sets the gauss of the magnet
+    private void SetMagnetGaussValue()
+    {
+        //hard coded debug test
+        gauss = 20;
+    }
+
+    //Configures the Sphere Collider for magneticFieldSphere
+    private void ConfigureSphereCollider()
+    {
+        magneticFieldSphere.isTrigger = true;
+        SetMagneticFieldRadius();
+        SetMagnetGaussValue();
+    }
+}
---
+++ Assets/Scripts/PhysicalProperties.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalProperties : MonoBehaviour
+{
+    ///A Class that defines basic physical properties of a Game Object
+    ///
+
+    //Properties (should be publicly accessible as these define how Game Objects can interact with each other)
+    public bool isPermanentMagnet;
+    public bool isMagnetic;
+
+
+    private void Start()
+    {
+        CheckMagnet();
+    }
+
+    //Methods
+    //A Method to make a Game Object a Magnet
+    private void MakeMagnet()
+    {
+        //GameObject magnetObject = new GameObject();
+        //Adds the Magnetism Componenent to the Game Object
+        gameObject.AddComponent<Magnetism>();
+
+    }
+    //A method to check if a Game Object should be a Magnet
+    private void CheckMagnet()
+    {
+        if (isPermanentMagnet)
+        {
+            MakeMagnet();
+        }
+    }
+}
